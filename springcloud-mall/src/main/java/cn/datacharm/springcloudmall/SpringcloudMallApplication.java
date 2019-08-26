0c7c136d2bbb224d889ebb5706439416b23ff696
Herb Fei <feihb123@vip.qq.com>
2019-08-26 23:57:44 +0800

    init

---
+++ springcloud-mall/src/main/java/cn/datacharm/springcloudmall/SpringcloudMallApplication.java
@@ -0,0 +1,13 @@
+package cn.datacharm.springcloudmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudMallApplication.class, args);
+    }
+
+}
---
+++ springcloud-mall/src/test/java/cn/datacharm/springcloudmall/SpringcloudMallApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.datacharm.springcloudmall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudMallApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
