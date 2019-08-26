f851ad38ea72fa820fce67eba561caf442d4144f
Herb Fei <feihb123@vip.qq.com>
2019-08-27 00:00:59 +0800

    eureka-server

---
+++ mall-eureka-server01/src/main/java/cn/datacharm/malleurekaserver01/MallEurekaServer01Application.java
@@ -0,0 +1,15 @@
+package cn.datacharm.malleurekaserver01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MallEurekaServer01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallEurekaServer01Application.class, args);
+    }
+
+}
